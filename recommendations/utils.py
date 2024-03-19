203d12f001bf79caa72277fd934d9173605c52bc
FabricioHiury <fabhiury@gmail.com>
2024-03-18 22:21:00 -0300

    Feat: implementing search movie by id and list similarities

--- recommendations/utils.py
+++ recommendations/utils.py
@@ -20,3 +20,25 @@ def search_movies(query):
     }
     response = requests.get(url, params=params)
     return response.json()
+
+def search_movie_by_id(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    params = {
+        'api_key': settings.TMDB_API_KEY,
+        'language': 'en-US',
+    }
+    response = requests.get(url, params=params)
+    if response.ok:
+        return response.json()
+    else:
+        return None
+    
+def get_genre_mapping():
+    url = "https://api.themoviedb.org/3/genre/movie/list"
+    params = {
+        "api_key": settings.TMDB_API_KEY,
+        "language": "en-US"
+    }
+    response = requests.get(url, params=params)
+    genres = response.json().get('genres', [])
+    return {genre['id']: genre['name'] for genre in genres}
